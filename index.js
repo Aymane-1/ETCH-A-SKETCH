aba76ff90301dca683dea5368205a1cd5fa3d7fb
Aeymne Echafii <aechafii@e1r2p13.1337.ma>
2022-03-25 02:44:28 +0100

    add features

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-let cont = document.getElementById("container");
-let numOfContainers = 16;
-function createGrid(){
-    for(let i = 0; i <= numOfContainers; i++)
-        cont.innerHTML += `<div></div>`;
-        console.log(i);
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = "white";
+const x = DEFAULT_SIZE;
+const y = DEFAULT_SIZE;
 
+const cont = document.getElementById("container");
+
+function createGrid(){
+    for(let i = 0; i < x * y; i++){
+        const box = document.createElement("div");
+        box.className = "cell"
+        box.innerHTML += '<div height: 25px; width: 25px"></div>';   
+        box.addEventListener('mouseover', setColor);
+        cont.appendChild(box);
+    }
+}
+
+function setColor(box){
+    box.target.style.backgroundColor = DEFAULT_COLOR;
+    box.target.style.cursor = "cell";
+}
+    
+function eraser(box){
+    box.target.style.backgroundColor = "black";
 }
 
 createGrid();
+// setColor();
